f505a3f872bf717e94bb8fcf2a71679e6220bdfd
Eugene Todoruk <nighteugene@ya.ru>
2025-10-30 23:17:09 +0300

    [about] Add AuroraOS port info

--- game/about.lua
+++ game/about.lua
@@ -7,6 +7,9 @@ local about = {}
 local url = "https://github.com/plinkr/flash-blip"
 local urlBounds = {}
 
+local url2 = "https://github.com/NightEugene/aurora-flash-blip"
+local url2Bounds = {}
+
 local mitUrl = "https://github.com/plinkr/flash-blip?tab=MIT-1-ov-file"
 local mitBounds = {}
 
@@ -39,15 +42,28 @@ function about.draw()
   mitBounds = { x = mitX, y = mitY, width = line1Width, height = mitHeight }
 
   local line2 = "INSPIRED BY THE WORK OF KENTA CHO"
-  Text.drawCenteredText(line2, Settings.WINDOW_HEIGHT * 0.70, 3.3)
+  Text.drawCenteredText(line2, Settings.WINDOW_HEIGHT * 0.65, 3.3)
 
   love.graphics.setColor(Colors.neon_lime_splash)
   local urlWidth = Text.getTextWidth(url, 2.9)
   local urlHeight = Text.charHeight * 2.9
   local urlX = (Settings.WINDOW_WIDTH - urlWidth) / 2
-  local urlY = Settings.WINDOW_HEIGHT * 0.75
+  local urlY = Settings.WINDOW_HEIGHT * 0.70
   urlBounds = { x = urlX, y = urlY, width = urlWidth, height = urlHeight }
   Text.drawText(url, urlX, urlY, 2.9)
+
+  if love.system.getOS() == "AuroraOS" then
+    local line3 = "PORTED TO AURORA OS BY NIGHTEUGENE"
+    Text.drawCenteredText(line3, Settings.WINDOW_HEIGHT * 0.75, 3.1)
+
+    love.graphics.setColor(Colors.neon_lime_splash)
+    local url2Width = Text.getTextWidth(url2, 2.1)
+    local url2Height = Text.charHeight * 2.1
+    local url2X = (Settings.WINDOW_WIDTH - url2Width) / 2
+    local url2Y = Settings.WINDOW_HEIGHT * 0.80
+    url2Bounds = { x = url2X, y = url2Y, width = url2Width, height = url2Height }
+    Text.drawText(url2, url2X, url2Y, 2.1)
+  end
 
   love.graphics.setColor(Colors.light_blue_glow)
   local gameVersionWidth = Text.getTextWidth(GAME_VERSION, 2)
@@ -85,6 +101,18 @@ function about.mousepressed(x, y, button)
     then
       love.system.openURL(mitUrl)
       return true -- Opens the project's MIT license URL
+    end
+
+    if love.system.getOS() == "AuroraOS" then
+      if
+        x > url2Bounds.x
+        and x < url2Bounds.x + url2Bounds.width
+        and y > url2Bounds.y
+        and y < url2Bounds.y + url2Bounds.height
+      then
+        love.system.openURL(url2)
+        return true -- Opens the URL
+      end
     end
   end
   return false -- Not clicked on URL, handled in main.lua
